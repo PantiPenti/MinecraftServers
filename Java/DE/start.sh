b6b23c1973920b3b26c2e8eecc89f78bb311c09b
PantiPenti <PantiPenti@qq.com>
2023-01-17 04:28:23 +0000

    更新start.sh

--- Java/DE/start.sh
+++ Java/DE/start.sh
@@ -1,2 +1,2 @@
 clear
-java -jar start.jar -javaagent:authlib-injector-1.2.1.jar=https://littleskin.cn/api/yggdrasil
+java -Xmx2048M -Xms2048M -jar start.jar -javaagent:authlib-injector-1.2.1.jar=https://littleskin.cn/api/yggdrasil
